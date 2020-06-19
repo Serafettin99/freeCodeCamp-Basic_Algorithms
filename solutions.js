b19d56396f30e502e072b731caf65c6b1b49011c
Serafettin Bayram <sbayram2016@gmail.com>
2020-06-19 22:51:39 +0200

    exercise 11 done, completed %81

--- solutions.js
+++ solutions.js
@@ -170,4 +170,14 @@ titleCase("I'm a little tea pot");
 
 // ==============================
 
-// 11 -
+// 11 - Basic Algorithm Scripting: Slice and Splice
+// You are given two arrays and an index.
+// Use the array methods slice and splice to copy each element of the first array into the second array, in order.
+// Begin inserting elements at index n of the second array.
+// Return the resulting array. The input arrays should remain the same after the function runs.
+
+function frankenSplice(arr1, arr2, n) {
+  const newArr2 = [...arr2];
+  return [...arr2.slice(0, n), ...arr1, ...newArr2.splice(n)];
+}
+frankenSplice([1, 2, 3], [4, 5, 6], 1);
